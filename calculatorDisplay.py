3011984df4698cd21b44f1af31d55986f8f7f649
Grahambros <43829500+Grahambros@users.noreply.github.com>
2020-06-25 19:58:11 -0500

    mostly cleanup

--- calculatorDisplay.py
+++ calculatorDisplay.py
@@ -8,10 +8,8 @@ import os
 import calculatorMain
 
 a = 0
-b = 1
+b = 100
 c = 0
-
-equalspressed = False
 
 class Buttonfunctions():
 
@@ -25,141 +23,113 @@ class Buttonfunctions():
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=1
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=1
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button2(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=2
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=2
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button3(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=3
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=3
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button4(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=4
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=4
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button5(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=5
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=5
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button6(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=6
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=6
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button7(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=7
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=7
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button8(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=8
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=8
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button9(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=9
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=9
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def button0(self):
         global b
         global a
         global c
-        if b <= 0:
-            b +=1
-        else:
-            a *= 10
-            a +=0
-            self.w = Label(root, text=(str(a)))
-            self.w.place(x=240, y = 50)
-            print(str(a))
+        a *= 10
+        a +=0
+        self.w = Label(root, text=(str(a)))
+        self.w.place(x=240, y = 50)
+        print(str(a))
 
     def equalbutton(self):
         global a
         global b
         global c
+        print(str(a+b))
         self.calcmain.calculate(a,c)
         self.w = Label(root, text=(str(a)))
         self.w.place(x=240, y = 50)
    
    
+#i need a way to disable the numbered buttons when the equals button is pressed
 class ButtonObjects():
     def __init__(self):
         self.Buttons = Buttonfunctions()
@@ -167,7 +137,7 @@ class ButtonObjects():
         self.myargs = [self.Buttons.button0, self.Buttons.button1, self.Buttons.button2, self.Buttons.button3, self.Buttons.button4, self.Buttons.button5, self.Buttons.button6, self.Buttons.button7, self.Buttons.button8, self.Buttons.button9]
 
     def buttons(self, root):
-
+        numberedbutton = Button()
         for x in range(1, 4):
             for y in range(1,4):
                 self.i+=1
@@ -194,9 +164,8 @@ root.geometry("500x540")
 gui = ButtonObjects()
 gui.buttons(root)
 
+
 root.update_idletasks()
-
-
 
 
 # Positions the window in the center of the page.
